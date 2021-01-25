89ae7dff663a215a98261d8b5d63ad8add86a73c
michael <michael@mike-linux-01.PK5001Z>
2021-01-25 14:25:21 -0500

    02-14

--- mooc-java-programming-i/part02-Part02_14.Counting/src/main/java/Counting.java
+++ mooc-java-programming-i/part02-Part02_14.Counting/src/main/java/Counting.java
@@ -1,10 +1,12 @@
-
 import java.util.Scanner;
-
 public class Counting {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        int userInput = Integer.valueOf(scanner.nextLine());
+        for (int i = 0; i <= userInput; i++) {
+            System.out.println(i);
+        }
     }
 }
